e64a4aafb4857f6d6775f5d317834ad047177d93
xanmoy <xanmoy.tsx@gmail.com>
2024-10-16 21:50:07 +0530

    updated

--- configuration.nix
+++ configuration.nix
@@ -48,7 +48,7 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
+  services.flatpak.enable = true;
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
